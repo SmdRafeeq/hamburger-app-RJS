4e0120f155e33f67fe370ecebf909bd2135a2da8
rafeeq <smrafeeq780@gmail.com>
2023-08-23 13:20:22 +0000

    Hamburger menu application

---
+++ src/components/Home/index.js
@@ -0,0 +1,13 @@
+import Header from "../Header"
+
+import "./index.css"
+
+const Home = () => (
+    <div className="home-container">
+        <Header />
+        <div className="home-content">
+            <img src="https://assets.ccbp.in/frontend/react-js/home-lg-img.png" className="home-img" alt="Home" />
+        </div>
+    </div>
+)
+export default Home
